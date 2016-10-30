d862c63cf7eaab0f299e79182f0b453dc866b1ae
krisnotchris <krishenderson@gmail.com>
2016-10-29 20:37:28 -0500

    Done.

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,5 +1,7 @@
 function ContactController() {
-    this.contacts = [{
+    var vm = this;
+
+    vm.contacts = [{
         name: 'Bob',
         phone: '0123458690'
     },{
@@ -10,9 +12,18 @@ function ContactController() {
         phone: '0684059433'
     }];
 
-    this.removeContact = function (index) {
-        this.contacts.splice(index, 1);
+    vm.removeContact = function (index) {
+        vm.contacts.splice(index, 1);
     }
+
+    vm.submit = function() {
+        vm.contacts.push({name: vm.name, phone: vm.phoneNumber})
+        vm.name = '';
+        vm.phoneNumber = '';
+    }
+    
+
+    
 }
 
 angular
